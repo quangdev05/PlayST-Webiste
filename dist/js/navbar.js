fe1a16d3009d0f22f6c3a6a6ecbe5a7214762dcb
SnakeVN <duyquang6991@gmail.com>
2024-07-15 14:27:43 +0700

    ...

--- dist/js/navbar.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const hamburger = document.querySelector('.hamburger');
-    const navLinks = document.querySelector('.nav-links');
-
-    hamburger.addEventListener('click', () => {
-        navLinks.classList.toggle('active');
-    });
-});
